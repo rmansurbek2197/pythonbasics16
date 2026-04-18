a9cc7006349b0ff38e4c0776069bdda4ee09f0e1
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:30:21 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-toiplam = {1, 2, 3, 4, 5}
-kvadratli_toiplam = {x**2 for x in toiplam}
-print(kvadratli_toiplam)
+def top_sonlar():
+    N = int(input("N ni kiriting: "))
+    sonlar = []
+    for i in range(1, N+1):
+        if i % 3 == 0 and i % 5 == 0:
+            sonlar.append(i)
+    print("1 dan", N, "gacha 3 va 5 ga bo'linadigan sonlar: ", sonlar)
+
+def asosiy():
+    print("1 dan N gacha 3 va 5 ga bo'linadigan sonlarni topish dasturi")
+    top_sonlar()
+    savol = input("Dasturni qayta ishga tushirishni hohlaysizmi? (ha/yo'q): ")
+    if savol.lower() == "ha":
+        asosiy()
+    else:
+        print("Dastur to'xtatildi")
+
+asosiy()
